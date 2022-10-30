7500cb274fa808c5c681eee8ab814fe2ce566709
IsuruRaveen20 <isururaveen4520@gmail.com>
2022-10-30 21:00:24 +0530

    changes to frontend

--- backend/app/Http/Controllers/Api/v1/ContactController.php
+++ backend/app/Http/Controllers/Api/v1/ContactController.php
@@ -10,24 +10,44 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
+    /*
+    -Index function
+    -Load All contacts
+    */
     public function index(){
         return ContactResource::collection(Contact::all());
     }
 
+    /*
+    -Show function
+    -Display contact
+    */
     public function show(Contact $contact){
         return new ContactResource($contact);
     }
 
+    /*
+    -store function
+    -Store contacts
+    */
     public function store(StoreContactRequest $request){
         Contact::create($request->validated());
         return response()->json("Contact Created");
     }
 
+    /*
+    -update function
+    -Update contact
+    */
     public function update(StoreContactRequest $request, Contact $contact){
         $contact->update($request->validated());
         return response()->json("Contact Updated!");
     }
 
+    /*
+    -Delete function
+    -delete a contact
+    */
     public function destroy(Contact $contact){
         $contact->delete();
         return response()->json("Contact Deleted!");
--- backend/app/Http/Controllers/Api/v1/ContactGroupController.php
+++ backend/app/Http/Controllers/Api/v1/ContactGroupController.php
@@ -10,24 +10,44 @@ use Illuminate\Http\Request;
 
 class ContactGroupController extends Controller
 {
+    /*
+    -Index function
+    -Load All contactGroups
+    */
     public function index(){
         return ContactGroupResource::collection(ContactGroup::all());
     }
 
+    /*
+    -Show function
+    -Display contactGroup
+    */
     public function show(ContactGroup $contactGroup){
         return new ContactGroupResource($contactGroup);
     }
 
+    /*
+    -store function
+    -Store contactGroup
+    */
     public function store(StoreContactGroupRequest $request){
         ContactGroup::create($request->validated());
         return response()->json("Contact Group Created");
     }
 
+    /*
+    -update function
+    -Update contactGroup
+    */
     public function update(StoreContactGroupRequest $request, ContactGroup $contactGroup){
         $contactGroup->update($request->validated());
         return response()->json("Contact Group Updated!");
     }
 
+    /*
+    -Delete function
+    -delete a contactGroup
+    */
     public function destroy(ContactGroup $contactGroup){
         $contactGroup->delete();
         return response()->json("Contact Group Deleted!");
