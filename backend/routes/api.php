5b8b094a250d5e313d885f248209a3b9385afa96
IsuruRaveen20 <isururaveen4520@gmail.com>
2022-10-30 19:08:00 +0530

    apis working

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\v1\ContactController;
+use App\Http\Controllers\Api\v1\ContactGroupController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +15,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1'], function(){
+    Route::apiResource('contacts', ContactController::class);
 });
+
+Route::group(['prefix' => 'v1'], function(){
+    Route::apiResource('contactGroups', ContactGroupController::class);
+});
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
