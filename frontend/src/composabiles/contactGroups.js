cca215510d2525d2f78c8a059a914a44d0f05ba3
IsuruRaveen20 <isururaveen4520@gmail.com>
2022-10-30 23:52:41 +0530

    changes

--- frontend/src/composabiles/contactGroups.js
+++ frontend/src/composabiles/contactGroups.js
@@ -4,7 +4,7 @@ import { useRouter } from 'vue-router';
 
 axios.defaults.baseURL = "http://127.0.0.1:8000/api/v1";
 
-export default function useContacts(){
+export default function useContactGroups(){
     
     const contactGroups = ref([]);
     const contactGroup = ref([]);
--- frontend/src/views/contactGroups/ContactGroupCreate.vue
+++ frontend/src/views/contactGroups/ContactGroupCreate.vue
@@ -1,62 +1,28 @@
 <script setup>
 import { reactive } from 'vue';
-import useContacts from '../../composabiles/contacts';
+import useContactGroups from '../../composabiles/contactGroups';
 
-const { storeContact, errors } = useContacts();
+const { storeContactGroup, errors } = useContactGroups();
 
 const form = reactive({
-    firstName: "",
-    lastName: "",
-    mobileNo: "",
-    email: "",
+    name: "",
 })
 </script>
 <template>
     <div class="mt-12">
         <div class="px-12">
             <form class="max-w-xl mx-auto p-5 bg-white show-md round-md border border-gray-300 rounded"
-                @submit.prevent="storeContact(form)">
+                @submit.prevent="storeContactGroup(form)">
                 <div class="space-y-6">
                     <label for="contacts" class="block mb-2 text-xl font-medium text-black dark:text-black">Create
-                        Contact</label>
+                        Contact Group</label>
                     <div class="mb-6">
-                        <label for="firstName" class="block mb-2 text-sm font-medium text-black dark:text-black">First
-                            Name</label>
-                        <input type="text" id="firstName" v-model="form.firstName"
+                        <label for="name" class="block mb-2 text-sm font-medium text-black dark:text-black">Name</label>
+                        <input type="text" id="name" v-model="form.name"
                             class="bg-white border border-gray-500 text-black text-sm rounded-lg focus:ring-blue-50 focus:border-blue-50 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-800 dark:text-black dark:focus:ring-blue-50 dark:focus:border-blue-50">
-                        <div v-if="errors.firstName">
-                            <span class="text-sm text-red-400">{{ errors.firstName[0] }}</span>
+                        <div v-if="errors.name">
+                            <span class="text-sm text-red-400">{{ errors.name[0] }}</span>
                         </div>
-                    </div>
-                    <div class="mb-6">
-                        <label for="lastName" class="block mb-2 text-sm font-medium text-black dark:text-black">Last
-                            Name</label>
-                        <input type="text" id="lastName" v-model="form.lastName"
-                            class="bg-white border border-gray-500 text-black text-sm rounded-lg focus:ring-blue-50 focus:border-blue-50 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-800 dark:text-black dark:focus:ring-blue-50 dark:focus:border-blue-50">
-                        <div v-if="errors.lastName">
-                            <span class="text-sm text-red-400">{{ errors.lastName[0] }}</span>
-                        </div>
-                    </div>
-                    <div class="mb-6">
-                        <label for="mobile" class="block mb-2 text-sm font-medium text-black dark:text-black">Mobile
-                            Number</label>
-                        <input type="text" id="mobileNo" v-model="form.mobileNo"
-                            class="bg-white border border-gray-500 text-black text-sm rounded-lg focus:ring-blue-50 focus:border-blue-50 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-800 dark:text-black dark:focus:ring-blue-50 dark:focus:border-blue-50">
-                        <div v-if="errors.mobileNo">
-                            <span class="text-sm text-red-400">{{ errors.mobileNo[0] }}</span>
-                        </div>
-                    </div>
-                    <div class="mb-6">
-                        <label for="email"
-                            class="block mb-2 text-sm font-medium text-black dark:text-black">Email</label>
-                        <input type="text" id="email" v-model="form.email"
-                            class="bg-white border border-gray-500 text-black text-sm rounded-lg focus:ring-blue-50 focus:border-blue-50 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-800 dark:text-black dark:focus:ring-blue-50 dark:focus:border-blue-50">
-                        <div v-if="errors.email">
-                            <span class="text-sm text-red-400">{{ errors.email[0] }}</span>
-                        </div>
-                    </div>
-                    <div class="mt-4">
-                        <button class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded">Submit</button>
                     </div>
                 </div>
             </form>
